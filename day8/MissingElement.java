fe75f75a174aeb0b07ea84aeb708125e97045cd1
karthikn6 <karthikn6tester@gmail.com>
2025-05-28 14:07:59 +0530

    Update MissingElement.java

--- day8/MissingElement.java
+++ day8/MissingElement.java
@@ -1,12 +1,27 @@
 package week3.day8;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class MissingElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int array[] = {1, 2, 3, 4, 10, 6, 8};
+		int[] value = {1, 2, 3, 4, 10, 6, 8};
 		
+		List<Integer> ListNum = new ArrayList<Integer>();
+		for(int i:value) {
+			 ListNum.add(i);
 	}
-
-}
+		System.out.println("Given list of number: "+ListNum);
+		Collections.sort(ListNum);
+		
+		for (int i = 1; i < ListNum.get(ListNum.size()-1); i++) {
+		if(!ListNum.contains(i)) {
+		System.out.println("Missing Elements is: "+(+i));;
+		
+		 }
+	 }
+	
+	}}
